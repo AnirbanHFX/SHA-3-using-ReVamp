a6bcb246d8f76928ebe80c68c35a52bb91f9ec6c
Anirban Majumder <43907249+HalcyonicFalconX@users.noreply.github.com>
2019-05-22 12:04:16 +0700

    Update inp_gen.c

--- inp_gen.c
+++ inp_gen.c
@@ -1,10 +1,73 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* stringToBinary(char* s) {
+    if(s == NULL) return 0; /* no input string */
+    size_t len = strlen(s);
+    char *binary = malloc(len*8 + 1); // each char is one byte (8 bits) and + 1 at the end for null terminator
+    binary[0] = '\0';
+    for(size_t i = 0; i < len; ++i) {
+        char ch = s[i];
+        for(int j = 7; j >= 0; --j){
+            if(ch & (1 << j)) {
+                strcat(binary,"1");
+            } else {
+                strcat(binary,"0");
+            }
+        }
+    }
+    return binary;
+}
+
+char *sponge(char *bin, unsigned int b) { 
+
+   if (bin == NULL)
+      return 0;
+
+   unsigned int len = strlen(bin);
+   unsigned int bytes = len/8;
+   char t;
+
+   char *s = malloc((b+1)*sizeof(char));
+
+   // LSB bit numbering
+
+   for(int i=0; i<bytes; i++) {
+      for(int j=0; j<4; j++) {
+         t = bin[8*i+j];
+         bin[8*i+j] = bin[8*(i+1) - j - 1];
+         bin[8*(i+1) - j - 1] = t;
+      }
+   }
+
+   for(unsigned int i = 0; i<b; i++) {
+      
+      if (i < len) {
+         s[i] = bin[i];
+      }
+
+      else if (i == len) 
+         s[i] = '0';
+      else if (i == len+1)
+         s[i] = '1';
+      else if (i == len+2)
+         s[i] = '1';
+      else if (i == b-1)
+         s[i] = '1';
+      else
+         s[i] = '0';
+
+   }
+
+   s[b] = '\0';
+
+   return s;
+
+}
+
 
 int main() {
-
-   srand(time(NULL));
 
    char RC[24][64] =  { "0000000000000000000000000000000000000000000000000000000000000001",
                         "0000000000000000000000000000000000000000000000001000000010000010",
@@ -33,7 +96,28 @@ int main() {
                       };
 
    FILE *fp, *fp2;
-   fp = fopen("temp3.txt", "w");
+   fp = fopen("Keccak-f1600.inp", "w");
+
+   char *x, *out;
+   x = (char *) malloc(199*sizeof(char));
+   printf("Input string: ");
+   scanf("%s", x);
+
+   out = (char *) malloc((8*strlen(x)+1)*sizeof(char));
+
+   out = stringToBinary(x);
+
+   printf("Bin: %s\n\n", out);
+
+   char *sp = sponge(out, 1600);
+
+   for(int i=0; i<25; i++) {
+
+      for(int j=0; j<64; j++) 
+         printf("%c", sp[64*i + j]);
+      printf("\n");
+
+   }
 
    for(int i=1; i<=100; i++) {
 
@@ -42,14 +126,7 @@ int main() {
          fprintf(fp, "%d ", i);
 
          for (int j=0; j<64; j++) {
-
-            int t = rand()%1000;
-
-            if (t > 500)
-               fprintf(fp, "1");
-            else
-               fprintf(fp, "0");
-
+            fprintf(fp, "%c", sp[64*(i/4) + j]);
          }
 
       fprintf(fp, "\n\n");
